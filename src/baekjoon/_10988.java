75930b6f1c395cf30040a058a3d739519719d9a3
blacksw1102 <blacksw1102@gmail.com>
2024-11-21 23:56:59 +0900

    add: 241121

--- src/baekjoon/_10988.java
+++ src/baekjoon/_10988.java
@@ -1,2 +1,25 @@
-package baekjoon;public class _10988 {
+package baekjoon;
+
+import java.util.Scanner;
+
+public class _10988 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        char[] a = scan.nextLine().toCharArray();
+
+        int b = 0;
+        int c = a.length;
+        int d = c / 2 + 1;
+        boolean e = true;
+        for (int i = 0; i < d; i++) {
+            if (a[i] != a[c - i - 1]) {
+                e = false;
+                break;
+            }
+        }
+
+        System.out.println((e) ? "1" : "0");
+
+        scan.close();
+    }
 }
--- src/baekjoon/_1157.java
+++ src/baekjoon/_1157.java
@@ -1,2 +1,30 @@
-package baekjoon;public class _1157 {
+package baekjoon;
+
+import java.util.Scanner;
+
+public class _1157 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        char[] a = scan.nextLine().toLowerCase().toCharArray();
+
+        int[] b = new int[26];
+        for (int i = 0; i <a.length; i++) {
+            b[a[i] - 97]++;
+        }
+
+        int c = 0;
+        boolean d = false;
+        for (int i = 1; i < b.length; i++) {
+            if (b[c] < b[i]) {
+                c = i;
+                d = false;
+            } else if (b[c] == b[i]) {
+                d = true;
+            }
+        }
+
+        System.out.println((d) ? "?" : (char)(65 + c));
+
+        scan.close();
+    }
 }
