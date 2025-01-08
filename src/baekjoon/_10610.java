6004d2019ad5fa744bf2bbb59bea0259b6e5a592
blacksw1102 <blacksw1102@gmail.com>
2025-01-08 23:31:54 +0900

    add: 20250108

--- src/baekjoon/_10610.java
+++ src/baekjoon/_10610.java
@@ -1,64 +1,24 @@
 package baekjoon;
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
 
-/**
- * 1. 입력받는 자릿 수들로 구성 가능한 수들을 구해야 한다.
- * 2. 1번에서 구한 수들 중 30의 배수가 되는 가장 큰 수를 구해야 한다.
- */
 public class _10610 {
-    static String[] valueArr;
-    static String[] trackArr;
-    static boolean[] visitedArr;
-    static Set<Integer> subSet;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        valueArr = br.readLine().split("");
-        trackArr = new String[valueArr.length];
-        visitedArr = new boolean[valueArr.length];
-        subSet = new TreeSet<>(Comparator.reverseOrder());
 
-        for (int i = 1; i <= valueArr.length; i++) {
-            initSubSet(i, 0);
-        }
+        String[] strs = br.readLine().split("");
+        Arrays.sort(strs, Comparator.reverseOrder());
+        String str = String.join("", strs);
 
-        int result = getResult();
+        boolean condition10 = strs[strs.length - 1].equals("0");
+        boolean condition3 = Arrays.stream(strs).mapToInt(Integer::parseInt).sum() % 3 == 0;
 
-        System.out.println(result);
-    }
-
-    public static int getResult() {
-        int result = -1;
-
-        for (Integer value : subSet) {
-            if (value % 30 == 0) {
-                result = value;
-                break;
-            }
-        }
-
-        return result;
-    }
-
-    public static void initSubSet(int length, int depth) {
-        if (depth < length) {
-            for (int i = 0; i < valueArr.length; i++) {
-                if (!visitedArr[i]) {
-                    visitedArr[i] = true;
-                    trackArr[depth] = valueArr[i];
-                    initSubSet(length, depth + 1);
-                    visitedArr[i] = false;
-                }
-            }
-        } else {
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < length; i++) {
-                sb.append(trackArr[i]);
-            }
-            subSet.add(Integer.parseInt(sb.toString()));
-        }
+        System.out.println(condition10 && condition3 ? str : "-1");
     }
 
 }
